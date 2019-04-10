41da1876892308029ef19727e22de57770ebae98
chisl <chisl@fenbi.com>
2019-04-10 23:48:24 +0800

    demo1

--- demo.js
+++ demo.js
@@ -1,3 +1,19 @@
+console.log('start');
+const p = new Promise((resolve, reject) => {
+  resolve()
+})
 setTimeout(() => {
   console.log('timer')
 }, 0);
+p.then(() => {
+  console.log('promise 1')
+  setTimeout(() => {
+    console.log('timer 2')
+  }, 0)
+  return new Promise((resolve) => {
+    resolve()
+  })
+}).then(() => {
+  console.log('promise 2')
+})
+console.log('end');
