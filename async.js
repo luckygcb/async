15258baed989716bf479efed09855852796648c1
chisl <chisl@fenbi.com>
2019-04-14 13:35:51 +0800

    demo

---
+++ async.js
@@ -0,0 +1,10 @@
+// 返回值和错误处理
+async function f() {
+  await new Promise(function (resolve, reject) {
+    throw new Error('出错了');
+  });
+}
+
+f()
+.then(v => console.log(v))
+.catch(e => console.log(e))
---
+++ promise.js
@@ -0,0 +1,63 @@
+// demo 1
+// const p = new Promise((resolve, reject) => {
+//   // reject('error')
+//   setTimeout(() => {
+//     resolve(1)
+//   }, 0)
+// })
+// p.then((value) => {
+//   console.log(value);
+// }, (e) => {
+//   console.log(e);
+// })
+// p.then((value) => {
+//   console.log(value);
+// }, (e) => {
+//   console.log(e);
+// })
+
+// demo2
+// 'use strict'
+// const p = new Promise((resolve, reject) => {
+//   resolve(1)
+// })
+// global.name = '123'
+// function foo(value) {
+//   console.log(this.name, value);
+// }
+// p.then(foo)
+
+// then方法返回
+// const p = new Promise((resolve, reject) => {
+//   resolve(1)
+// })
+// const p1 = new Promise((resolve, reject) => {
+//   resolve(1)
+// })
+// p.then(() => {
+//   return {
+//     error: 'error',
+//     then: function(resolveP, rejectP) {
+//       rejectP(this.error);
+//     }
+//   }
+// }).then((value) => {
+//   console.log(value)
+// }, (e) => {
+//   console.log(e)
+// })
+
+// promise.all promise.race
+// const promises = [2, 3, 5, 7, 11, 13];
+
+// Promise.all(promises).then(function (posts) {
+//   console.log(posts);
+// }).catch(function(reason){
+//   console.log(reason)
+// });
+
+// Promise.race(promises).then(function (posts) {
+//   console.log(posts);
+// }).catch(function(reason){
+//   console.log(reason)
+// });
